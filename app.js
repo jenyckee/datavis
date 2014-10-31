df413321a9eb2d5495ef50e568e8c0544013fdff
Jeroen Nyckees <jenyckee@dhcp-148-4.vub.ac.be>
2014-10-31 14:43:39 +0100

    connection to mongo and cleaning data

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,114 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+var recordSchema = mongoose.Schema(
+{
+	id: Number,
+	stamp: Number,
+	stmt1: Number,
+	stmt2: Number,
+	stmt3: Number,
+	stmt4: Number,
+	stmt5: Number,
+	stmt6: Number,
+	stmt7: Number,
+	stmt8: Number,
+	stmt9: Number,
+	stmt10: Number,
+	stmt11: Number,
+	stmt12: Number,
+	stmt13: Number,
+	stmt14: Number,
+	stmt15: Number,
+	stmt16: Number,
+	stmt17: Number,
+	stmt18: Number,
+	stmt19: Number,
+	stmt20: Number,
+	stmt21: Number,
+	stmt22: Number,
+	stmt23: Number,
+	stmt24: Number,
+	stmt25: Number,
+	stmt26: Number,
+	stmt27: Number,
+	stmt28: Number,
+	stmt29: Number,
+	stmt30: Number,
+	stmt31: Number,
+	stmt32: Number,
+	stmt33: Number,
+	stmt34: Number,
+	stmt35: Number,
+	stmt36: Number,
+	cat1: Number,
+	cat2: Number,
+	cat3: Number,
+	cat4: Number,
+	cat5: Number,
+	cat6: Number,
+	cat7: Number,
+	cat8: Number,
+	cat9: Number,
+	cat10: Number,
+	cat11: Number,
+	cat12: Number,
+	stelling1: Boolean,
+	stelling2: Boolean,
+	stelling3: Boolean,
+	stelling4: Boolean,
+	stelling5: Boolean,
+	stelling6: Boolean,
+	stelling7: Boolean,
+	stelling8: Boolean,
+	stelling9: Boolean,
+	stelling10: Boolean,
+	stelling11: Boolean,
+	stelling12: Boolean,
+	stelling13: Boolean,
+	stelling14: Boolean,
+	stelling15: Boolean,
+	stelling16: Boolean,
+	stelling17: Boolean,
+	stelling18: Boolean,
+	stelling19: Boolean,
+	stelling20: Boolean,
+	stelling21: Boolean,
+	stelling22: Boolean,
+	stelling23: Boolean,
+	stelling24: Boolean,
+	stelling25: Boolean,
+	stelling26: Boolean,
+	stelling27: Boolean,
+	stelling28: Boolean,
+	stelling29: Boolean,
+	stelling30: Boolean,
+	stelling31: Boolean,
+	stelling32: Boolean,
+	stelling33: Boolean,
+	stelling34: Boolean,
+	stelling35: Boolean,
+});
+
+var Record = mongoose.model('record', recordSchema, 'record');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+	Record.findOne({id:1}, function (err, record) {
+	  if (err) throw err;
+	  // console.log(record)
+	});
+
+	res.render('index', {
+		title: 'Express'
+	});
 });
 
 module.exports = router;
