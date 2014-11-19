8275fa9df895f09997f2cf27562b201d9bb086cf
Jeroen Nyckees <jenyckee@jenyckee.local>
2014-11-19 11:49:06 +0100

    clean code etc.

--- public/javascripts/sequences.js
+++ public/javascripts/sequences.js
@@ -1,6 +1,6 @@
 // Dimensions of sunburst.
-var width = 750;
-var height = 600;
+var width = 300;
+var height = 300;
 var radius = Math.min(width, height) / 2;
 
 // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
--- public/javascripts/vis.js
+++ public/javascripts/vis.js
@@ -27,7 +27,7 @@ function histoGram(fD) {
 
 
     console.log(fD)
-        
+
     var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
     hGDim.w = 500 - hGDim.l - hGDim.r, 
     hGDim.h = 300 - hGDim.t - hGDim.b;
@@ -92,26 +92,6 @@ function histoGram(fD) {
     
     return hG;
 }
-var freqData=[
-{State:'AL',freq:{low:4786, mid:1319, high:249}}
-,{State:'AZ',freq:{low:1101, mid:412, high:674}}
-,{State:'CT',freq:{low:932, mid:2149, high:418}}
-,{State:'DE',freq:{low:832, mid:1152, high:1862}}
-,{State:'FL',freq:{low:4481, mid:3304, high:948}}
-,{State:'GA',freq:{low:1619, mid:167, high:1063}}
-,{State:'IA',freq:{low:1819, mid:247, high:1203}}
-,{State:'IL',freq:{low:4498, mid:3852, high:942}}
-,{State:'IN',freq:{low:797, mid:1849, high:1534}}
-,{State:'KS',freq:{low:162, mid:379, high:471}}
-];
-
 
 histoGram(stmts);
-    
-
-// var pies = d3.select("#statements-wrapper")
-//              .selectAll("svg")
-//              .data(stmts)
-//              .enter()
-//              .append("g");
     
